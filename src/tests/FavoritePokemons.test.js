7308ec0a0b4f85577c9a2d597f1e94390008b6c5
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2022-10-19 19:11:07 -0300

    correção de lint em favoritepokemons.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { act } from 'react-dom/test-utils';
 import renderWithRouter from './renderWithRouter';
 // import FavoritePokemons from '../pages/FavoritePokemons';
 import App from '../App';
