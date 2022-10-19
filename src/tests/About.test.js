9a56f324d8396738b572cc35d6c717a801b48bea
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2022-10-19 18:22:16 -0300

    resolução do requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import About from '../pages/About';
+
+describe('testa o componente about', () => {
+  test('a página contém as informações sobre a Pokédex', () => {
+    render(<About />);
+    const pokedexInfo = screen.getByText('This application simulates a Pokédex, a digital encyclopedia containing all Pokémons');
+
+    expect(pokedexInfo).toBeInTheDocument();
+  });
+
+  test('se é exibido na tela um h2 com texto About Pokédex', () => {
+    render(<About />);
+    const h2About = screen.getByRole('heading', { level: 2, name: 'About Pokédex' });
+    expect(h2About).toBeInTheDocument();
+  });
+
+  test('se o atributo src da imagem é', () => {
+    render(<About />);
+    const pokedex = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    const image = screen.getByRole('img');
+    expect(image).toHaveAttribute('src', pokedex);
+    // https://stackoverflow.com/questions/6050952jestreact-native-testing-library-how-to-test-an-image-src
+  });
+});
